86d6040dde66a593676faf1560fc9f01b2732aa9
mike_p <mike.polynin@gmail.com>
2021-09-11 16:08:08 +0300

    mod01

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,34 +1,48 @@
 import datetime
+import random
+
 from flask import Flask
 
 app = Flask(__name__)
 
+counter = 0
 
-@app.route('/hello_word')
-def test_function():
-   pass
+
+@app.route('/counter')
+def counter_func():
+    global counter
+    counter += 1
+    return 'This page was opened {0} times'.format(counter)
+
+
+@app.route('/hello_world')
+def hello_world_func():
+    return 'Привет, мир!'
 
 
 @app.route('/cars')
-def test_function():
-   pass
+def cars_func():
+    return 'Chevrolet, Renault, Ford, Lada'
 
 
 @app.route('/cats')
-def test_function():
-   pass
+def cats_func():
+    return random.choice(['корниш рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин'])
 
 
 @app.route('/get_time/now')
-def test_function():
-   pass
+def get_time_now_func():
+    return 'Точное время {current_time}'.format(current_time=datetime.datetime.now().time())
 
 
 @app.route('/get_time/future')
-def test_function():
-   pass
+def get_time_future_func():
+    return 'Точное время через час будет {current_time_after_hour}'.format(
+        current_time_after_hour=(datetime.datetime.now() + datetime.timedelta(hours=1)).time())
 
 
 @app.route('/get_random_word')
-def test_function():
-   pass
+def get_random_word_func():
+    with open('module_01_flask/homework/war_and_peace.txt', 'r', encoding='utf-8') as war_and_peace_text:
+        all_words = war_and_peace_text.read().split()
+    return random.choice(all_words)
