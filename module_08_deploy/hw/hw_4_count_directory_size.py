e35c2a8a4780af1dfeab2128a30b999f920b2ba7
mike_p <mike.polynin@gmail.com>
2021-12-08 00:40:23 +0300

    hw08

--- module_08_deploy/hw/hw_4_count_directory_size.py
+++ module_08_deploy/hw/hw_4_count_directory_size.py
@@ -10,10 +10,28 @@
 В случае, если на вход функции передаётся несуществующий путь или НЕ директория,
     функция должна выкинуть исключение ValueError с красивым описание ошибки
 """
-
+import os
 from pathlib import Path
 from typing import Union
 
+size = 0
+
 
 def calculate_directory_size(directory_path: Union[str, Path] = ".") -> int:
-    pass
+    global size
+
+    if isinstance(directory_path, str) or isinstance(directory_path, Path):
+        address = os.path.abspath(directory_path)
+    else:
+        raise ValueError('Неверный ввод директории')
+
+    if os.path.exists(address):
+        for elem in os.listdir(address):
+            if os.path.isdir(os.path.join(address, elem)):
+                calculate_directory_size(os.path.join(address, elem))
+            else:
+                size += os.path.getsize(os.path.join(address, elem))
+    else:
+        raise ValueError('Неверный ввод директории')
+
+    return size
