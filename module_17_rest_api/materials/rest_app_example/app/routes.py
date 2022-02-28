172fa9dacced9e26440a66b613fa3c068edc0350
Skillbox <it@skillbox.ru>
2022-02-28 08:09:54 +0000

    Add lesson

---
+++ module_17_rest_api/materials/rest_app_example/app/routes.py
@@ -0,0 +1,44 @@
+from typing import Tuple, List, Dict
+
+from flask import Flask, request
+from flask_restful import Api, Resource
+from marshmallow import ValidationError
+
+from models import (
+    DATA,
+    get_all_books,
+    init_db,
+    add_book,
+)
+from schemas import BookSchema
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class BookList(Resource):
+    def get(self) -> Tuple[List[Dict], int]:
+        schema = BookSchema()
+        return schema.dump(get_all_books(), many=True), 200
+
+    def post(self) -> Tuple[Dict, int]:
+
+        data = request.json
+
+        schema = BookSchema()
+
+        try:
+            book = schema.load(data)
+        except ValidationError as exc:
+            return exc.messages, 400
+
+        book = add_book(book)
+        return schema.dump(book), 201
+
+
+api.add_resource(BookList, '/api/books')
+
+
+if __name__ == '__main__':
+    init_db(initial_records=DATA)
+    app.run(debug=True)
