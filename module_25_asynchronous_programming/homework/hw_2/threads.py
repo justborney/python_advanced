fc51fba716bef3bd907256f955f44269810025c7
mike_p <mike.polynin@gmail.com>
2022-05-21 21:51:41 +0200

    hw25

---
+++ module_25_asynchronous_programming/homework/hw_2/threads.py
@@ -0,0 +1,40 @@
+import threading
+import time
+from pathlib import Path
+
+import requests as requests
+
+URL = 'https://cataas.com/cat'
+CATS_WE_WANT = 10
+OUT_PATH = Path(__file__).parent / 'cats'
+OUT_PATH.mkdir(exist_ok=True, parents=True)
+OUT_PATH = OUT_PATH.absolute()
+
+
+def get_cat(idx: int) -> None:
+    response = requests.get(URL)
+    print(response.status_code)
+    result = response.content
+    write_to_disk(result, idx)
+
+
+def write_to_disk(content: bytes, id: int) -> None:
+    file_path = "{}/{}.png".format(OUT_PATH, id)
+    with open(file_path, mode='wb') as f:
+        f.write(content)
+
+
+def main():
+    threads = [threading.Thread(get_cat(i)) for i in range(CATS_WE_WANT)]
+    for thread in threads:
+        thread.start()
+        thread.join()
+    print(len(threads))
+    time_finish = time.time()
+    work_time = time_finish - time_start
+    print(work_time)
+
+
+if __name__ == '__main__':
+    time_start = time.time()
+    main()
